5ecc711c8130d84c5376aa8b015cdcca4e902562
Adhya Sharma <adhyasharma1806@gmail.com>
2026-03-12 01:46:47 +0530

    fix signup api endpoint

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -22,7 +22,7 @@ export default function SignIn() {
 
     try{
 
-      const res = await fetch("http://localhost:5000/api/auth/login",{
+      const res = await fetch("https://voyageai-1-b0iw.onrender.com/api/auth/signin",{
         method:"POST",
         headers:{
           "Content-Type":"application/json"
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -23,7 +23,7 @@ export default function SignUp() {
 
     try{
 
-      const res = await fetch("http://localhost:5000/api/auth/signup",{
+      const res = await fetch("https://voyageai-1-b0iw.onrender.com/api/auth/signup",{
         method:"POST",
         headers:{
           "Content-Type":"application/json"
